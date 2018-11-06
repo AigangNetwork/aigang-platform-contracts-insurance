413896557e0a3c58594784718ed3411eed9c551b
pauliax <pauliax6@gmail.com>
2018-11-06 22:40:46 +0200

    Refactor equal function

--- contracts/utils/Strings.sol
+++ contracts/utils/Strings.sol
@@ -2,19 +2,6 @@ pragma solidity ^0.4.23;
 
 library Strings {
     function equal(string memory _a, string memory _b) pure internal returns (bool) {
-        bytes memory a = bytes(_a);
-        bytes memory b = bytes(_b);
-       
-        if (a.length != b.length) {
-            return false;
-        }
-        
-        for (uint i = 0; i < a.length; i ++) {
-            if (a[i] != b[i]) {
-                return false;
-            }
-        }
-        
-        return true;
+        return (sha3(_a) == sha3(_b));
     }
 }
