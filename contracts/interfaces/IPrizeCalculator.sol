97af76dace84b844646da3db11ffd13cb95010ce
DarDev <6444878+dariusdev@users.noreply.github.com>
2018-11-15 09:42:46 +0200

    extract pool contracts to other repo

--- contracts/interfaces/IPrizeCalculator.sol
+++
@@ -1,8 +0,0 @@
-pragma solidity ^0.4.23;
-
-interface IPrizeCalculator {
-    function calculatePrizeAmount(uint _predictionTotalTokens, uint _winOutputTotalTokens, uint _forecastTokens)
-        pure
-        external
-        returns (uint);
-}
